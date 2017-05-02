bd5964d5dadc6aace67bf52428c4816b21dce19f
avabin <andrzej.piotrowski76@gmail.com>
2017-05-02 19:41:50 +0200

    Created database integrity test

---
+++ src/test/java/tk/avabin/tdg/DatabaseIntegrityTest.java
@@ -0,0 +1,40 @@
+package tk.avabin.tdg;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.postgresql.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by avabin on 02.05.17.
+ */
+public class DatabaseIntegrityTest {
+    private String databaseURL;
+    private String databaseUsername;
+    private String databasePass;
+
+    @Before
+    public void beforeDatabaseTest() {
+        databaseURL = System.getenv("DATABASE_URL_T");
+        databaseUsername = System.getenv("DATABASE_USERNAME");
+        databasePass = System.getenv("DATABASE_PASSWORD");
+    }
+
+    @Test
+    public void databaseTest() {
+        try {
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+
+            if (driver.acceptsURL(databaseURL)) {
+                Connection conn = DriverManager.getConnection(databaseURL, databaseUsername, databasePass);
+            }
+        } catch (SQLException e) {
+            System.err.println("Could not load driver!");
+            System.exit(1);
+        }
+    }
+}
