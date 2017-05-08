4682cb33c3a12d2129fc0ebc863d77e82e7d54f0
avabin <andrzej.piotrowski76@gmail.com>
2017-05-08 14:23:09 +0200

    Configuring SSL connection

--- src/main/java/tk/avabin/tdg/beans/Services/ApplicationPropertiesService.java
+++ src/main/java/tk/avabin/tdg/beans/Services/ApplicationPropertiesService.java
@@ -2,7 +2,6 @@ package tk.avabin.tdg.beans.Services;
 
 import lombok.Data;
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,14 +11,9 @@ import org.springframework.stereotype.Service;
 @Log
 @Service
 public class ApplicationPropertiesService {
-    @Value("$(server.ssl.key-store-password:none)")
-    private String SSLPassword;
 
     public ApplicationPropertiesService() {
-        log.info(SSLPassword);
-        this.SSLPassword = System.getenv("SSL_PASSWORD");
-        log.info(SSLPassword);
-        log.info(System.getenv("SSL_PASSWORD"));
+
     }
 
 }
