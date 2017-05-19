42a1d9be4eeadfae85f4f484c20b9f1fa7b0d2b7
Avabin <andpio002@utp.edu.pl>
2017-05-19 09:30:41 +0200

    Deploy

--- src/main/java/tk/avabin/tdg/beans/Services/DTO/Implementations/GenericDtoServiceImpl.java
+++ src/main/java/tk/avabin/tdg/beans/Services/DTO/Implementations/GenericDtoServiceImpl.java
@@ -2,11 +2,13 @@ package tk.avabin.tdg.beans.Services.DTO.Implementations;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tk.avabin.tdg.beans.Services.DTO.GenericDtoService;
 
 /**
  * Created by Avabin on 18.05.2017.
  */
+@Service
 public class GenericDtoServiceImpl implements GenericDtoService {
     @Autowired
     private ModelMapper modelMapper;
