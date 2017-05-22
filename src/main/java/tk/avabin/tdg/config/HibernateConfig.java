4c153bb737fd5c1879e5c0aee9ad541da7b36dd4
avabin <andrzej.piotrowski76@gmail.com>
2017-05-22 14:54:25 +0200

    Deploy

--- src/main/java/tk/avabin/tdg/config/HibernateConfig.java
+++ src/main/java/tk/avabin/tdg/config/HibernateConfig.java
@@ -38,8 +38,8 @@ public class HibernateConfig {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl(System.getenv("DATABASE_URL_T"));
-        dataSource.setUsername(System.getenv("POSTGRESQL_USER"));
-        dataSource.setPassword(System.getenv("POSTGRESQL_PASSWORD"));
+        dataSource.setUsername(System.getenv("DATABASE_USER"));
+        dataSource.setPassword(System.getenv("DATABASE_PASSWORD"));
 
         return dataSource;
     }
