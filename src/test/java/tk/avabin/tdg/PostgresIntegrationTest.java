d10532d429e7a4be230590954eb2974402a27a16
Avabin <andrzej.piotrowski76@gmail.com>
2017-09-08 22:00:18 +0200

    testing Circle CI

--- src/test/java/tk/avabin/tdg/PostgresIntegrationTest.java
+++ src/test/java/tk/avabin/tdg/PostgresIntegrationTest.java
@@ -5,9 +5,11 @@ import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostgresIntegrationTest {
 
     @Autowired
