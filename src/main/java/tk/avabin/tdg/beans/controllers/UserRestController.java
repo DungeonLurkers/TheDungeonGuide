a9a571b45f2367c860add368c34d4be65687f97b
apiotrow <andrzej.piotrowski.ext@nokia.com>
2017-09-13 15:42:43 +0200

    Working on PSQL connection

--- src/main/java/tk/avabin/tdg/beans/controllers/UserRestController.java
+++ src/main/java/tk/avabin/tdg/beans/controllers/UserRestController.java
@@ -1,11 +1,29 @@
 package tk.avabin.tdg.beans.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tk.avabin.tdg.beans.dtos.UserDto;
+import tk.avabin.tdg.beans.entities.User;
+import tk.avabin.tdg.beans.services.Entities.UserService;
 
 /**
  * Created by Avabin on 18.05.2017.
  */
 @RestController
+@RequestMapping("/api/user")
 public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public User addUser(@RequestBody UserDto object) {
+        User user = mapper.map(object, User.class);
+        return user;
+    }
 
 }
