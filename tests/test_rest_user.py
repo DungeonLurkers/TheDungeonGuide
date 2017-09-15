333301b698c31f593d0f6fa7cd93d472cf9b8231
apiotrow <andrzej.piotrowski.ext@nokia.com>
2017-09-15 15:33:50 +0200

    Testing usable of RxPython within project

--- tests/test_rest_user.py
+++ tests/test_rest_user.py
@@ -1,5 +1,16 @@
-import requests
-import rx
+import yaml
+from rx import Observable
+
+
+def execute_request(request):
+    responseRequires = Observable.from_(request["response"])
+    print("------------")
+    print("Message: ", request["message"])
+    print("Method:", request["method"])
+    print("Payload: ", request["payload"])
+    print("Response: ", request["response"])
+
+    responseRequires.subscribe(lambda v: print(v))
 
 
 class RequestParser:
@@ -7,5 +18,19 @@ class RequestParser:
         self.template = None
         self.actions = None
 
-    def set_template(self, template):
-        self.template = template
+    def set_template(self, template_file):
+        self.template = template_file
+        self.actions = Observable.from_(yaml.load(open(template_file, "r")))
+
+    def execute(self):
+        self.actions.subscribe(execute_request)
+
+
+def main():
+    p = RequestParser()
+    p.set_template("templates/rest_user.yml")
+    p.execute()
+
+
+if __name__ == '__main__':
+    main()
